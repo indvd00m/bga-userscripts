19c8c41d37959619df5e44f9bdeac6e94565d1ea
indvd00m <gotoindvdum@gmail.com>
2024-09-22 21:48:42 +0300

    Release v0.2.1

--- kingdom-builder.bga.user.js
+++ kingdom-builder.bga.user.js
@@ -3,7 +3,7 @@
 // @description Автоматический подсчет игровых параметров
 // @author indvd00m <gotoindvdum [at] gmail [dot] com>
 // @license Creative Commons Attribution 3.0 Unported
-// @version 0.2.0-dev
+// @version 0.2.1
 // @match https://boardgamearena.com/*/kingdombuilder*
 // ==/UserScript==
 
