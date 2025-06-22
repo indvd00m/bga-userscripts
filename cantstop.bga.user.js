817375a49d0cda4edc19196df787a3d968339c62
indvd00m <gotoindvdum@gmail.com>
2025-06-22 17:16:11 +0300

    Refactor calculations

--- cantstop.bga.user.js
+++ cantstop.bga.user.js
@@ -434,69 +434,96 @@ var cantStopBgaUserscriptData = {
         const nMax = progressState.saveProgressNMax50PercentSuccess;
         const spExpectation = progressState.saveProgressExpectation;
         const rdCount = progressState.rollingDiceCount;
+        const minDangerZoneFactor = 0.85;
+        let scaleMaxValue = nMax * 2;
+        if (spExpectation > 0 && spExpectation / minDangerZoneFactor > scaleMaxValue) {
+            scaleMaxValue = Math.ceil(spExpectation / minDangerZoneFactor);
+        }
 
-        const stateJsonElement = `<span style="font-size: 60%; font-family: monospace; white-space: pre-wrap;">${JSON.stringify(progressState, null, 4)}</span>`;
-        const formattedNMax = this.formatDecimal(nMax, 0);
-        const formattedExpectation = this.formatDecimal(spExpectation, 0);
-        const formattedSaveProgressProbability = this.formatDecimal(progressState.saveProgressProbability * 100, 2);
-        const nMaxPercentage = 50;
-        const expectationPercentage = nMax === 0 ? 100 : (nMax === Infinity ? 100 : (100 * spExpectation / (nMax * 2)));
+        let nMaxPercentage;
+        let expectationPercentage;
+        let nonlinearRDCountPercentage = 0;
         let rdCountPercentage = 0;
         let nextRDCountPercentage = 0;
-        if (spExpectation === 0) {
+        if (scaleMaxValue === 0) {
+            nMaxPercentage = 50;
+            expectationPercentage = 100;
+            nonlinearRDCountPercentage = 0;
             rdCountPercentage = 0;
             nextRDCountPercentage = 0;
-        } else if (rdCount < spExpectation) {
-            rdCountPercentage = nMax === 0 ? 100 : (nMax === Infinity ? 0 : (100 * rdCount / (nMax * 2)));
-            nextRDCountPercentage = nMax === 0 ? 100 : (nMax === Infinity ? 0 : (100 * (rdCount + 1) / (nMax * 2)));
+        } else if (scaleMaxValue === Infinity) {
+            nMaxPercentage = 100 * nMax / scaleMaxValue;
+            expectationPercentage = 100;
+            nonlinearRDCountPercentage = 0;
+            rdCountPercentage = 0;
+            nextRDCountPercentage = 0;
+        } else if (scaleMaxValue > 0) {
+            nMaxPercentage = 100 * nMax / scaleMaxValue;
+            expectationPercentage = 100 * spExpectation / scaleMaxValue;
+            if (rdCount < spExpectation) {
+                nonlinearRDCountPercentage = 100 * rdCount / (nMax * 2);
+                rdCountPercentage = 100 * rdCount / scaleMaxValue;
+                nextRDCountPercentage = 100 * (rdCount + 1) / scaleMaxValue;
+            } else {
+                const k = 0.2;
+                const rdCountDangerValue = spExpectation + (scaleMaxValue - spExpectation) * (1 - 2.71828 ** (-k * (rdCount - spExpectation)));
+                const nextRDCountDangerValue = spExpectation + (scaleMaxValue - spExpectation) * (1 - 2.71828 ** (-k * (rdCount + 1 - spExpectation)));
+                nonlinearRDCountPercentage = 100 * rdCountDangerValue / scaleMaxValue;
+                rdCountPercentage = 100 * rdCountDangerValue / scaleMaxValue;
+                nextRDCountPercentage = 100 * (nextRDCountDangerValue) / scaleMaxValue;
+            }
         } else {
-            const k = 0.2;
-            const rdCountDangerValue = spExpectation + (nMax * 2 - spExpectation) * (1 - 2.71828 ** (-k * (rdCount - spExpectation)));
-            const nextRDCountDangerValue = spExpectation + (nMax * 2 - spExpectation) * (1 - 2.71828 ** (-k * (rdCount + 1 - spExpectation)));
-            rdCountPercentage = nMax === 0 ? 100 : (nMax === Infinity ? 0 : (100 * rdCountDangerValue / (nMax * 2)));
-            nextRDCountPercentage = nMax === 0 ? 100 : (nMax === Infinity ? 0 : (100 * (nextRDCountDangerValue) / (nMax * 2)));
+            nMaxPercentage = 50;
+            expectationPercentage = 100;
+            rdCountPercentage = 0;
+            nextRDCountPercentage = 0;
         }
         let color = 'green';
         switch (true) {
             case (rdCountPercentage >= expectationPercentage):
                 color = '#330000';
                 break;
-            case (rdCountPercentage <= 10):
+            case (nonlinearRDCountPercentage <= 10):
                 color = '#33FF99';
                 break;
-            case (rdCountPercentage <= 20):
+            case (nonlinearRDCountPercentage <= 20):
                 color = '#00FF99';
                 break;
-            case (rdCountPercentage <= 30):
+            case (nonlinearRDCountPercentage <= 30):
                 color = '#33CC66';
                 break;
-            case (rdCountPercentage <= 40):
+            case (nonlinearRDCountPercentage <= 40):
                 color = '#00CC66';
                 break;
-            case (rdCountPercentage <= 50):
+            case (nonlinearRDCountPercentage <= 50):
                 color = '#009933';
                 break;
-            case (rdCountPercentage <= 55):
+            case (nonlinearRDCountPercentage <= 55):
                 color = '#FFCC00';
                 break;
-            case (rdCountPercentage <= 60):
+            case (nonlinearRDCountPercentage <= 60):
                 color = '#FF9933';
                 break;
-            case (rdCountPercentage <= 65):
+            case (nonlinearRDCountPercentage <= 65):
                 color = '#CC3333';
                 break;
-            case (rdCountPercentage <= 70):
+            case (nonlinearRDCountPercentage <= 70):
                 color = '#993333';
                 break;
-            case (rdCountPercentage <= 80):
+            case (nonlinearRDCountPercentage <= 80):
                 color = '#990033';
                 break;
-            case (rdCountPercentage <= 90):
+            case (nonlinearRDCountPercentage <= 90):
                 color = '#330000';
                 break;
             default:
-                color = 'green';
+                color = '#330000';
         }
+
+        const formattedNMax = this.formatDecimal(nMax, 0);
+        const formattedExpectation = this.formatDecimal(spExpectation, 0);
+        const formattedSaveProgressProbability = this.formatDecimal(progressState.saveProgressProbability * 100, 2);
+
         const stateProgressBarElement =
             `<div class="progressbar_with_info">` +
             `   <div class="progressbar_inner">` +
@@ -523,8 +550,9 @@ var cantStopBgaUserscriptData = {
             `       </di>` +
             `   </div>` +
             `</div>`;
-        // const stateElement = `${stateJsonElement}${stateProgressBarElement}`;
-        const stateElement = `${stateProgressBarElement}`;
+        const stateJsonElement = `<span style="font-size: 60%; font-family: monospace; white-space: pre-wrap;">${JSON.stringify(progressState, null, 4)}</span>`;
+        const stateElement = `${stateJsonElement}${stateProgressBarElement}`;
+        // const stateElement = `${stateProgressBarElement}`;
         this.dojo.place(stateElement, PROGRESS_STATE_PANEL_ID, 'only');
     },
 
