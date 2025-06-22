aa522d1176797ee0244d679538828c91cb6ee675
indvd00m <gotoindvdum@gmail.com>
2025-06-22 18:38:04 +0300

    Dev version

--- cantstop.bga.user.js
+++ cantstop.bga.user.js
@@ -3,7 +3,7 @@
 // @description Extended statistics for Can't Stop game at BGA
 // @author indvd00m <gotoindvdum [at] gmail [dot] com>
 // @license Creative Commons Attribution 3.0 Unported
-// @version 1.3.0
+// @version 1.4.0-dev
 // @match https://boardgamearena.com/*/cantstop*
 // @match https://*.boardgamearena.com/*/cantstop*
 // @grant none
