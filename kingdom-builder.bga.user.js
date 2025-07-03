e831eae6904b19b6aa8d135abdac3ba506ccbcf1
indvd00m <gotoindvdum@gmail.com>
2025-07-03 23:38:32 +0300

    New versions

--- kingdom-builder.bga.user.js
+++ kingdom-builder.bga.user.js
@@ -3,7 +3,7 @@
 // @description Extended statistics for Kingdom Builder game at BGA
 // @author indvd00m <gotoindvdum [at] gmail [dot] com>
 // @license Creative Commons Attribution 3.0 Unported
-// @version 0.9.5
+// @version 0.9.6
 // @match https://boardgamearena.com/*/kingdombuilder*
 // @grant none
 // @updateURL https://github.com/indvd00m/bga-userscripts/raw/refs/heads/master/kingdom-builder.bga.user.js
