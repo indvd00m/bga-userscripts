fd9e4d6f02a02eba53ea765c883ff8b4e7bc437e
indvd00m <gotoindvdum@gmail.com>
2025-09-21 15:59:07 +0300

    Fixed long game variant calculation

--- cantstop.bga.user.js
+++ cantstop.bga.user.js
@@ -21,7 +21,8 @@ const CS_LOGS_DESKTOP_ELEMENT_ID = "logs";
 const CS_LOGS_MOBILE_ELEMENT_ID_PREFIX = "chatwindowlogs_zone_tablelog_";
 const CS_LOG_DESKTOP_MESSAGE_ID_PREFIX = "log_CS_desktop_";
 const CS_LOG_MOBILE_MESSAGE_ID_PREFIX = "log_CS_mobile_";
-const CS_DEFAULT_MAX_CHIPS_COUNT = 3;
+const CS_DEFAULT_WIN_CHIPS_COUNT = 3;
+const CS_DEFAULT_BLACK_CHIPS_COUNT = 3;
 const CS_DICE_SELECT_ID_PREFIX = "dice_select_";
 const CS_PROGRESS_STATE_PANEL_ID = "progress_state";
 const CS_LINE_PROBABILITIES_PANEL_ID = "line_probabilities";
@@ -76,7 +77,8 @@ var cantStopBgaUserscriptData = {
     lineProbabilities: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     currentLineCounts: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     bgaTableId: 0,
-    maxChipsCount: CS_DEFAULT_MAX_CHIPS_COUNT,
+    winChipsCount: CS_DEFAULT_WIN_CHIPS_COUNT,
+    blackChipsCount: CS_DEFAULT_BLACK_CHIPS_COUNT,
     rollDiceLog: [],
     parseLog: false,
 
@@ -99,7 +101,7 @@ var cantStopBgaUserscriptData = {
             this.myPlayerId = parseInt(myPlayerId);
         }
         if (this.game.required_column_count != null) {
-            this.maxChipsCount = parseInt(this.game.required_column_count);
+            this.winChipsCount = parseInt(this.game.required_column_count);
         }
 
         const stats = this.calcStats();
@@ -504,7 +506,7 @@ var cantStopBgaUserscriptData = {
                     !sums.every(s => closedColumnNumbers.includes(s))
                     && (
                         args.some(a => sums.includes(a))
-                        || args.length < this.maxChipsCount
+                        || args.length < this.blackChipsCount
                     )
             ).length;
     },
